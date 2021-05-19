850d55037c1825f0a236fa5a34af0c1cbabf196f
911mateo911 <malajmateo@gmail.com>
2021-05-19 22:28:10 +0200

    created with create-react-app added color palette

---
+++ src/Palette/Palette.js
@@ -0,0 +1,15 @@
+import React, { Component } from 'react'
+
+export default class Palette extends Component {
+    render() {
+        return (
+            <div className='palette' >
+                {/* Navbar here */}
+                <div className='palette-colors' >
+                     {/* Color boxes */}
+                </div>
+                {/* Footer here */}
+            </div>
+        )
+    }
+}
